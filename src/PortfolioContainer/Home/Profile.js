93288040251ff98b3b75c06aed5c2d143b8a9cac
Teun Jansen <teunjansen91@gmail.com>
2023-09-01 09:13:02 +0200

    first component icons

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,27 @@
+export default function Portfolio() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.facebook.com">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.google.com">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="https://www.instagram.com">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.youtube.com">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+            <a href="https://www.twitter.com">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
